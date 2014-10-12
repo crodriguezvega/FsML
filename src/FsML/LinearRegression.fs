85827af1a39407157d839245514b5abb4d44e67e
Carlos Rodriguez <crodveg@yahoo.es>
2014-10-12 19:10:58 +0200

    First commit with linear regression.

---
+++ src/FsML/LinearRegression.fs
@@ -0,0 +1,21 @@
+﻿namespace FsML
+
+open MathNet.Numerics
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
+
+module LinearRegression =
+
+  /// Fit with gradient descent
+  let fitWithGradientDescent (trainingX: Matrix<_>) (trainingY: Vector<_>) learningRate numberOfiterations =
+    let rec loop i (theta: Vector<_>) =
+      match i with
+      | 0 -> theta
+      | _ ->
+        let derivative = trainingX.TransposeThisAndMultiply (trainingX * theta - trainingY)
+        loop (i - 1) (theta - (learningRate / float trainingX.ColumnCount) * derivative)
+    loop numberOfiterations (DenseVector(trainingX.ColumnCount))
+
+  /// Fit with normal equation
+  let fitWithNormalEquation (trainingX: Matrix<_>) (trainingY: Vector<_>) =
+    ((trainingX.TransposeThisAndMultiply trainingX).Inverse().TransposeAndMultiply trainingX) * trainingY
